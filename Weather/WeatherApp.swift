1e29259a8d10d28c8a2b89ddf05f6ad7a55d91d6
marcodellisola <marcodellisola@outlook.it>
2023-03-27 17:05:52 +0200

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Marco Dell'Isola on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
